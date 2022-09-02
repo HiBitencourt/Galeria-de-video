00b83a1001a3ad7ef4bec6684cf3d358a7f5afe9
HiBitencourt <hiemimik@gmail.com>
2022-09-02 15:03:24 -0300

    Galeria de vídeos simples

---
+++ script.js
@@ -0,0 +1,20 @@
+let videoPlayer = document.getElementById('videoPlayer');
+let myVideo = document.getElementById('myVideo');
+
+function stopVideo(){
+    myVideo = 'block';
+    videoPlayer.style.display = 'none';
+}        
+
+function playVideo(file){
+    myVideo.src = file;
+    videoPlayer.style.display = 'block';
+}
+
+// span.onclick = function () {
+//     modal.style.display = 'none';
+//     var myVideo = document.getElementById("myVideo");
+//     myVideo.src = 'Eternal.mp4';
+//   }
+  
+ 

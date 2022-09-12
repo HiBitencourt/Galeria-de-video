2a31a50046701be87cad5b12321d1232a8605bfd
HiBitencourt <hiemimik@gmail.com>
2022-09-12 18:31:23 -0300

    Alteração no script.js

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ let myVideo = document.getElementById('myVideo');
 function stopVideo(){
     myVideo = 'block';
     videoPlayer.style.display = 'none';
-}        
+};    
 
 function playVideo(file){
     myVideo.src = file;
     videoPlayer.style.display = 'block';
-}
+};
 
-// span.onclick = function () {
-//     modal.style.display = 'none';
-//     var myVideo = document.getElementById("myVideo");
-//     myVideo.src = 'Eternal.mp4';
-//   }
+span.onclick = function () {
+    modal.style.display = 'none';
+    var myVideo = document.getElementById("myVideo");
+    myVideo.src = 'Eternal.mp4';
+};
   
  
